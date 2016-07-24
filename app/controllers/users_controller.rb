09ba2975c403ed7cef04bd8169a1bd3cf4279a4a
Necrostic <damiong28@gmail.com>
2016-07-24 00:23:05 +0000

    Add show and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,25 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def show
+    @user=User.find(params[:id])
+  end
+  
+  def edit
+    @user=User.find(params[:id])
+  end
+  
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "User updated!"
+      redirect_to users_url
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
     def user_params
