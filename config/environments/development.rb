74fafee2849144f14087bbb64beae8e999b1a33f
Garry Gomez <ggomez@flexwage.com>
2018-08-05 20:33:40 -0700

    Add link to bill in summary mailer, fix development mailer default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = {host: 'https://billshare-necrostic.c9users.io', port: 2525 }
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000 }
 end
